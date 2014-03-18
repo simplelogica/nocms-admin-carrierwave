c47cc1a9a9490c6fcbdf4ca2de70eb7fb71fd933
David J. Brenes <david.brenes@simplelogica.net>
2014-03-18 18:36:37 +0100

    Index view created. Now we can check attachments are indeed created

--- app/controllers/no_cms/admin/carrierwave/attachments_controller.rb
+++ app/controllers/no_cms/admin/carrierwave/attachments_controller.rb
@@ -3,6 +3,10 @@ require_dependency "no_cms/admin/carrierwave/application_controller"
 module NoCms::Admin::Carrierwave
   class AttachmentsController < ApplicationController
 
+    def index
+      @attachments = NoCms::Carrierwave::Attachment.all
+    end
+
     def new
       @attachment = NoCms::Carrierwave::Attachment.new
     end
