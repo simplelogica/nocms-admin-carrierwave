0fe0b96695455530d32bd624879d8b7a00f6424c
David J. Brenes <david.brenes@simplelogica.net>
2014-03-18 17:56:10 +0100

    Created action and views for 'new' form

--- app/controllers/no_cms/admin/carrierwave/attachments_controller.rb
+++ app/controllers/no_cms/admin/carrierwave/attachments_controller.rb
@@ -2,5 +2,9 @@ require_dependency "no_cms/admin/carrierwave/application_controller"
 
 module NoCms::Admin::Carrierwave
   class AttachmentsController < ApplicationController
+
+    def new
+      @attachment = NoCms::Carrierwave::Attachment.new
+    end
   end
 end
