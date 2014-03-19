3f3bb1f1686ae3dd8b3a97959f9aafd08b5af9e2
David J. Brenes <david.brenes@simplelogica.net>
2014-03-19 12:42:43 +0100

    Improving images secondary menu

---
+++ app/helpers/no_cms/admin/carrierwave/attachments_helper.rb
@@ -0,0 +1,11 @@
+module NoCms::Admin::Carrierwave
+  module AttachmentsHelper
+
+    def attachment_listing_item_classes attachment, current_attachment
+      classes = []
+      classes << 'current' if current_attachment == attachment
+      classes
+    end
+
+  end
+end
